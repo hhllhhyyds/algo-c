287b0da57daa9f04d1a47853c3d47efe1e72efa4
Adventurer <2055976331@qq.com>
2024-05-21 22:43:47 +0800

    no change but test_vector_and_linkedlist

--- tests/test_vector_and_linkedlist.c
+++ tests/test_vector_and_linkedlist.c
@@ -1,6 +1,15 @@
 // 用for 循环同时遍历 LinkedList 和 Vector, 遍历过程中比较各自的元素是否相等
+#include <vector_and_linkedlist.h>
+#include <stdio.h>
 void test_vector_insert_linkedlist()
 {
+    struct Vector *new_example_v = new_example_vector();
+    struct LinkedList *list = vector_insert_linkedlist();
+    int i;
+    for (i = 0; i < 100; i++)
+    {
+        Elem a =
+    }
 }
 
 int main()
